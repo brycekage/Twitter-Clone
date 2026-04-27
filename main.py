84073491ca0d638a4ddb755d25c8c1df66993a89
brycekage <bkageyama29@cmc.edu>
2026-04-27 11:41:08 -0700

    Twitter Setup

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ Starts a hello world webserver
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
-    return 'hello <b>world</b>'
+    return templates.TemplateResponse(
+        request=request,
+        name='index.html',
+    )
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8080, reload=True)
+
+# Now we'e gonna load html from a file
+# So added jinja2 and template stuff
+# internal server error always means an internal python error inside of the function that 
+# corresponds to the rout or page that was connected
